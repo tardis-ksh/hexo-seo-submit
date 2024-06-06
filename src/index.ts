ca379c5d20c2d5efff6c3f96da7db544c35196eb
tardis <14971857@qq.com>
2024-06-06 14:50:34 +0800

    test: test eslint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
-const testFunc = () => {
+import * as path from 'node:path';
+
+const fn = (params: string) => {
+  console.log(params, path);
+  return 123;
+};
+
+const testFunc = (props) => {
+  console.log(props, fn('132'));
   return 'Hello World';
 };
 
