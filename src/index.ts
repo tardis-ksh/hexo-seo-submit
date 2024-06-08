6f473561c97ffe96c5cbd2af8cd8f30c334f2b73
tardis <14971857@qq.com>
2024-06-09 00:20:46 +0800

    feat: add baidu generator

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import chalk from 'chalk';
+import merge from 'lodash.merge';
 
 import { baiduGenerator, bingGenerator, googleGenerator } from '@/generators';
+import deploys from '@/deploys';
 
 import { PLUGIN_NAME, SearchEngines } from '@/constants';
 import { SeoHexoConfig } from '@/types';
+import IHexo from '@/types/hexo';
 
 console.log(chalk.bold.bgMagenta(`${PLUGIN_NAME} run, have fun!`));
 
@@ -12,17 +15,23 @@ const SearchEngineConfig = {
 };
 
 const DefaultConfig: SeoHexoConfig = {
-  [SearchEngines.BAIDU]: SearchEngineConfig,
-  [SearchEngines.BING]: SearchEngineConfig,
-  [SearchEngines.GOOGLE]: SearchEngineConfig,
+  [SearchEngines.BAIDU]: {
+    ...SearchEngineConfig,
+    path: 'baidu.txt',
+  },
+  [SearchEngines.BING]: {
+    ...SearchEngineConfig,
+    path: 'bing.json',
+  },
+  [SearchEngines.GOOGLE]: {
+    ...SearchEngineConfig,
+    path: 'google.txt',
+  },
 };
 
 console.log(hexo.config[PLUGIN_NAME], 'log kshao-1');
 
-hexo.config[PLUGIN_NAME] = Object.assign(
-  DefaultConfig,
-  hexo.config[PLUGIN_NAME],
-);
+hexo.config[PLUGIN_NAME] = merge({}, DefaultConfig, hexo.config[PLUGIN_NAME]);
 
 console.log(hexo.config[PLUGIN_NAME], 'log kshao-2');
 
@@ -43,3 +52,8 @@ hexo.extend.generator.register(
   `${PLUGIN_NAME}[${SearchEngines.GOOGLE}] generator`,
   googleGenerator,
 );
+
+// batch deployer
+hexo.extend.deployer.register(PLUGIN_NAME, async () => {
+  return await deploys(hexo as IHexo);
+});
