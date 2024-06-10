ae1be66dc8e39cb4f91108cbddd604b846ca3e8f
tardis <14971857@qq.com>
2024-06-10 22:46:59 +0800

    feat: add examples

--- examples/use-command/command.sh
+++ examples/use-command/command.sh
@@ -1,6 +1,14 @@
 # baidu
-$ npx hexo-seo-submit baidu -t your-token -s https://ksh7.com -f baidu.txt
+$ npx hexo-seo-submit baidu -t <your baidu token> -s https://ksh7.com -f baidu.txt
 
 # bing
 # must .json file, Eg: { siteUrl: 'https://ksh7.com', urlList: ['xxx'] }
-$ npx hexo-seo-submit bing -k you-api-key -f examples/files/bing.json
+$ npx hexo-seo-submit bing -k <you bing api key> -f examples/files/bing.json
+
+# google
+# use Environment variables
+$ export PRIVATE_KEY="-----BEGIN PRIVATE KEY-----\n..."
+$ npx hexo-seo-submit google -f examples/files/baidu.txt -p http://127.0.0.1:7890 -mail <your client_email here> -key "$PRIVATE_KEY"
+
+# use json file
+$ npx hexo-seo-submit google -f examples/files/baidu.txt -p http://127.0.0.1:7890 -kf examples/files/google-keys.json
