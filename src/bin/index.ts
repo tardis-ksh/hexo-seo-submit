c0d179ae727b2c0a63154562cbfd230d028548e9
tardis <14971857@qq.com>
2024-06-12 10:39:52 +0800

    feat: remove log

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -98,7 +98,6 @@ program
 
     try {
       const submitConfig: Partial<BatchSubmitConfig> = {};
-      console.log(private_key, 'private_key');
       // use props
       if (client_email && private_key) {
         submitConfig.accountKeysJSon = JSON.stringify({
